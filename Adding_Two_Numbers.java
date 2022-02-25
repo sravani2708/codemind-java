1cfb32ce48e5c0bbcecd43c8a61c8118423958cb
sravani2708 <69574493+sravani2708@users.noreply.github.com>
2022-02-25 18:18:43 +0530

    import java.util.*; class add {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n1=s.nextInt();         int n2=s.nextInt();         System.out.print(n1+n2);
        }
    }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class add
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n1=s.nextInt();
+        int n2=s.nextInt();
+        System.out.print(n1+n2);
+        
+    }
+}
